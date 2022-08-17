7b08c80948b21952c5ff1e8760885be8a0121fd3
汐殇 <95160953+xishang0128@users.noreply.github.com>
2022-08-17 10:38:33 +0800

    Create bstar.js

---
+++ surge/bstar.js
@@ -0,0 +1,9 @@
+let body = $request.body;
+
+body = body.replace(/&sim_code=\d+/,"&sim_code=52507");
+body = body.replace(/&locale=zh_CN/,"&locale=zh_SG");
+body = body.replace(/&s_locale=zh-Hans_[A-Z]{2}/,"&s_locale=zh-Hans_SG");
+
+console.log(body)
+
+$done({body});
