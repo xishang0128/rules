6e4660a1b9ef4e7b67c176c582676d7a3c03f83c
汐殇 <95160953+xishang0128@users.noreply.github.com>
2022-08-17 10:47:00 +0800

    Update bstar.js

--- surge/bstar.js
+++ surge/bstar.js
@@ -2,7 +2,7 @@ let body = $request.body;
 
 body[/&sim_code=\d+/] = "&sim_code=52507";
 body[/&locale=zh_CN/] = "&locale=zh_SG";
-body[/&s_locale=zh-Hans_[A-Z]{2}/] = "&s_locale=zh-Hans_SG");
+body[/&s_locale=zh-Hans_[A-Z]{2}/] = "&s_locale=zh-Hans_SG";
 
 console.log(body)
 
