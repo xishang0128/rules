773d4d8f84624f94015fc375f45aac25ed069b30
汐殇 <95160953+xishang0128@users.noreply.github.com>
2022-10-24 02:19:19 +0800

    Delete surge directory

--- surge/bstar.js
+++
@@ -1,9 +0,0 @@
-let body = $request.body;
-
-body body.replace[/&sim_code=\d+/] = "&sim_code=52507";
-body body.replace[/&locale=zh_CN/] = "&locale=zh_SG";
-body body.replace[/&s_locale=zh-Hans_[A-Z]{2}/] = "&s_locale=zh-Hans_SG";
-
-console.log(body)
-
-$done({body});
